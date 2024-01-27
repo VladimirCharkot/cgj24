2ad9f9da1b21dea11ede477c1eb9f115dc07e556
VladimirCharkot <vlad.chk@gmail.com>
2024-01-26 22:53:40 -0300

    fix dialogo?

--- dialogo.gd
+++ dialogo.gd
@@ -2,15 +2,14 @@ extends Node2D
 
 var texto = ""
 
+signal listo
+
 func _ready():
 	ocultar()
 
 func decir(p_texto):
 	texto = p_texto
 	$Timein.start()
-	
-func decidir():
-	pass
 	
 func mostrar():
 	visible = true
@@ -20,7 +19,8 @@ func ocultar():
 
 func _on_timer_timeout():
 	if texto:
-		$Texto.append_text(texto[0])
+		#$Texto.append_text(texto[0])
+		$Texto.text += texto[0]
 		texto = texto.right(texto.length() - 1)
 	else:
 		$Letras.stop()
@@ -31,9 +31,16 @@ func _on_timer_timeout():
 
 func _on_timeout_timeout():
 	ocultar()
+	$Timeout.stop()
+	listo.emit()
 
 
 func _on_timein_timeout():
 	mostrar()
 	$Timein.stop()
 	$Letras.start()
+	#listo.emit()
+
+
+func _on_listo():
+	$Texto.text = ""
